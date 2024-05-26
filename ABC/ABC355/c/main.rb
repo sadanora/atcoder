05a730a01e19fce9c906117d1ac639a97a7a7300
sadanora <sadanora.pi@gmail.com>
2024-05-26 23:07:17 +0900

    abc355

--- ABC/ABC355/c/main.rb
+++ ABC/ABC355/c/main.rb
@@ -1,13 +1,19 @@
 n, t = gets.split.map(&:to_i)
 a = gets.split.map(&:to_i)
-res = -1
-numarr = Array.new(n){Array.new(n, 0)}
+row, col = Array.new(2){Array.new(n, 0)}
+dia = [0, 0]
+a.each.with_index(1) { |num, idx|
+  i = num - 1
+  j = i / n
+  k = i % n
+  row[j] += 1
+  col[k] += 1
+  dia[0] += 1 if j == k
+  dia[1] += 1 if (j + k) == n - 1
 
-a.each { |i|
-  y = (i + 1) / n - 1
-  # x = (i % (n * (y+1))) - 1
-  x = (i % n) - 1
-  p [y, x]
-  numarr[y][x] = 1
+  if [row[j], col[k], dia].flatten.include?(n)
+    puts idx
+    exit
+  end
 }
-p numarr
+puts -1
