efdca6ce3cc859be2f11e48a76cefcf8c1553fbd
sadanora <sadanora.pi@gmail.com>
2024-05-31 23:31:54 +0900

    ABC299b

--- ABC/ABC299/b/main.rb
+++ ABC/ABC299/b/main.rb
@@ -1,5 +1,16 @@
-gets.to_i
-gets.split.map(&:to_i)
-arr = n.times.map{ gets.to_i }
-arr = n.times.map{ gets.split.map(&:to_i) }
-
+n, t = gets.split.map(&:to_i)
+c_arr = gets.split.map(&:to_i)
+r_arr = gets.split.map(&:to_i)
+cnt = 0
+c_arr.each { |c| cnt += 1 if c == t }
+color = cnt == 0 ? c_arr[0] : t 
+max = -1
+ans = -1
+n.times { |i|
+  next if c_arr[i] != color
+  if max < r_arr[i]
+    max = r_arr[i]
+    ans = i + 1
+  end
+}
+puts ans
