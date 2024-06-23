e8d2a44a82b160892b80c12d5af2a05af7ca7ae9
sadanora <sadanora.pi@gmail.com>
2024-06-24 00:03:15 +0900

    ABC310b

--- ABC/ABC310/b/main.rb
+++ ABC/ABC310/b/main.rb
@@ -1,22 +1,6 @@
-# n, m = gets.split.map(&:to_i)
-# pcf = n.times.map { gets.split.map(&:to_i) }.sort
-
-# (n-1).times {
-#   pi, _, *fi = pcf.shift
-#   pcf.each { |arr|
-#     pj, _, *fj = arr
-#     # next if fi.size <= fj.size
-
-#     if (fi & fj).size >= fi.size && (fi | fj).size > fj.size || pi < pj
-#       puts 'Yes'
-#       exit
-#     end
-#   }
-# }
-# puts 'No'
 require 'set'
 
-n, m = gets.split.map(&:to_i)
+n, = gets.split.map(&:to_i)
 p = []
 c = []
 f = []
@@ -29,8 +13,7 @@ end
 ans = false
 n.times do |i|
   n.times do |j|
-    ans |= p[i] >= p[j] && f[j].superset?(f[i]) && ([p[i], p[j], f[j].size].max > f[i].size)
+    ans ||= p[i] >= p[j] && f[j].superset?(f[i]) && ((p[i] > p[j]) || f[j].size > f[i].size)
   end
 end
 puts ans ? 'Yes' : 'No'
-# n.times.map { gets.split.map(&:to_i) }.sort
