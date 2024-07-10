60b917ecc50157496d4a4263cc925200c6cc6449
sadanora <sadanora.pi@gmail.com>
2024-07-10 16:51:04 +0900

    abc273b abc271a

--- ABC/ABC273/b/main.rb
+++ ABC/ABC273/b/main.rb
@@ -1,4 +1,11 @@
-gets.to_i
-gets.split.map(&:to_i)
-arr = n.times.map{ gets.to_i }
-arr = n.times.map{ gets.split.map(&:to_i) }
+x, k = gets.split.map(&:to_i)
+(0...k).each { |i|
+  x /= 10**i
+  mod = x % 10
+  if mod <= 4
+    x = (x - mod) * (10**i)
+  else
+    x = (x + (10 - mod)) * (10**i)
+  end
+}
+puts x
