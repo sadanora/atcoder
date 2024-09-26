0b0f3e21a630e63471624ad311cd1b0d33206914
sadanora <sadanora.pi@gmail.com>
2024-09-26 22:06:42 +0900

    abc083b

--- ABC/abc083/b/main.rb
+++ ABC/abc083/b/main.rb
@@ -1,9 +1,7 @@
 n, a, b = gets.split.map(&:to_i)
-puts (1..n).to_a.select { | i |
-  k = 0
-  while i > 0
-    k += i % 10
-    i /= 10
-  end
-  a <= k && k <= b
-}.sum
+ans = 0
+1.upto(n) do |i|
+  s = i.digits.sum
+  ans += i if a <= s && s <= b
+end
+puts ans
