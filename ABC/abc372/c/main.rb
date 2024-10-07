a468aadd223dff70af3a1c836d14a6bdfa13573e
sadanora <sadanora.pi@gmail.com>
2024-10-07 19:02:56 +0900

    abc372c

--- ABC/abc372/c/main.rb
+++ ABC/abc372/c/main.rb
@@ -1,3 +1,15 @@
 n, q = gets.split.map(&:to_i)
-s = gets.chomp
-arr = q.times.map{ gets.chomp.split }
+s = '..' + gets.chomp + '..'
+count = s.scan('ABC').size
+
+q.times.each do
+  x, c = gets.split
+  x = x.to_i
+
+  before = s[x-1..x+3].scan('ABC').size
+  s[x+1] = c
+  after = s[x-1..x+3].scan('ABC').size
+  diff = after - before
+  puts count + diff
+  count += diff
+end
