de706e4b04673238657b0a64fe101e55b21689fd
sadanora <sadanora.pi@gmail.com>
2024-10-11 20:18:45 +0900

    abc367a

--- ABC/abc367/a/main.rb
+++ ABC/abc367/a/main.rb
@@ -1,15 +1,11 @@
 a, b, c = gets.split.map(&:to_i)
-# n = b
-# while n != c
-#   if n == a
-#     puts 'No'
-#     exit
-#   end
-#   n += 1
-#   n %= 24
-# end
-# puts 'Yes'
-
-# puts (a-c) % 24 < b && b < (b-c) % 24 ? 'Yes' : 'No'
-
-puts (a-b)*(b-c)*(c-a) > 0 ? 'Yes' : 'No'
+now = b
+while now != c
+  if now == a
+    puts 'No'
+    exit
+  end
+  now += 1
+  now %= 24
+end
+puts 'Yes'
