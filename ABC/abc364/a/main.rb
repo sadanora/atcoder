e87aa0fe34754b3e11b2cef5a0b793dac9e79688
sadanora <sadanora.pi@gmail.com>
2024-10-18 00:23:21 +0900

    abc364a

--- ABC/abc364/a/main.rb
+++ ABC/abc364/a/main.rb
@@ -2,8 +2,22 @@ def rec(si, count)
   return true if count < 3
 
   sj = gets.chomp
-  si == 'sweet' && sj == 'sweet' ? false : rec(sj, count - 1)
+  (si + sj).eql?('sweetsweet') ? false : rec(sj, count - 1)
 end
 
 n = gets.to_i
 puts rec(gets.chomp, n) ? 'Yes' : 'No'
+
+# n = gets.to_i
+# si = gets.chomp
+# while n > 3
+#   sj = gets.chomp
+#   if (si + sj).eql?('sweetsweet')
+#     puts 'No'
+#     exit
+#   else
+#     si = sj
+#     n -= 1
+#   end
+# end
+# puts 'Yes'
