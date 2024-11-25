c743dc6f03508a70a6014b4c2fa221d1f33d1c6a
sadanora <sadanora.pi@gmail.com>
2024-11-25 21:49:54 +0900

    abc113c

--- ABC/abc113/c/main.rb
+++ ABC/abc113/c/main.rb
@@ -1,11 +1,10 @@
-N, M = gets.split.map(&:to_i)
-ary = Array.new(M){|i| [gets.split.map(&:to_i), i] }.sort
+n, m = gets.split.map(&:to_i)
+ary = m.times.map { [gets.split.map(&:to_i), _1] }.sort
 ans = []
-M.times.with_index{|i|
-  pf = ary.select{|a| a[0][0] == i + 1}
-  pf.each_with_index{|c,i|
-    ans << ["#{format("%06d", c[0][0])}#{format("%06d", i+1)}", c[1]]
+m.times.map do |i|
+  pf = ary.select { _1[0][0] == i + 1 }
+  pf.each_with_index { |c,i|
+    ans << ["#{c[0][0].to_s.rjust(6, '0')}#{(i+1).to_s.rjust(6, '0')}", c[1]]
   }
-}
-puts ans.sort_by{_2}.map(&:first)
-# ans.sort_by{|a,b| b}.each{puts _1[0]}
+end
+puts ans.sort_by{_2}.map { _1[0] }
