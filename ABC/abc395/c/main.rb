cbdfee96a9ed7a570ae300cb8ed2d456b57113a4
sadanora <sadanora.pi@gmail.com>
2025-03-17 20:08:20 +0900

    abc395

--- ABC/abc395/c/main.rb
+++ ABC/abc395/c/main.rb
@@ -1,12 +1,12 @@
 n = gets.to_i
 arr = gets.split.map(&:to_i)
-h = {}
-arr.each_with_index do |a,i|
-  if h[a].nil?
-    h[a] = [i, nil]
-  else
-    h[a] = [i, i - h[a][0] + 1]
+ans = n + 1
+pos = Array.new(1000001) { []}
+n.times { |i| pos[arr[i]] << i }
+1000001.times do |i|
+  next if pos[i].size < 2
+  (pos[i].size - 1).times do |j|
+    ans = [ans, pos[i][j+1] - pos[i][j] + 1].min
   end
 end
-min = h.values.transpose[1].compact.min
-puts min ? min : -1
+puts ans == n + 1 ? -1 : ans
