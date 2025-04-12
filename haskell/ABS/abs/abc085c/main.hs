51b8e90583a242a2f35149d862dde403145db2e6
sadanora <sadanora.pi@gmail.com>
2025-04-12 20:50:09 +0900

    abc085c

---
+++ haskell/ABS/abs/abc085c/main.hs
@@ -0,0 +1,8 @@
+main :: IO ()
+main = do
+  [n, y] <- map (read :: String -> Int) . words <$> getLine
+  let ans = [[a, b, c] | a <- [0..n],
+                         b <- [0..(n-a)],
+                         let c = n-a-b,
+                         10000*a + 5000*b + 1000*c == y]
+  putStrLn $ unwords $ map show $ if null ans then [-1, -1, -1] else head ans
