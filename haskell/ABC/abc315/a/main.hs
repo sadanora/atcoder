6d47ef99fb274d81f552ed96be603843b83348bc
sadanora <sadanora.pi@gmail.com>
2025-05-12 22:37:16 +0900

    NoviSteps 7Q 78

---
+++ haskell/ABC/abc315/a/main.hs
@@ -0,0 +1,34 @@
+import Control.Monad
+import Data.Bool (bool)
+import Data.Char
+import Data.List
+import Data.Ord
+import Data.Maybe
+import qualified Data.Set as Set
+import Text.Printf
+
+main :: IO ()
+main = do
+  s <- getLine
+  putStrLn $ [ x | x <- s, x `notElem` "aiueo" ]
+
+getInt :: IO Int
+getInt = readLn
+getInts :: IO [Int]
+getInts = toInt <$> getLine
+getContentsToInt :: IO [Int]
+getContentsToInt = toInt <$> getContents
+
+toInt :: String -> [Int]
+toInt = map (read :: String -> Int) . words
+toTuple :: [b] -> (b, b)
+toTuple [x, y] = (x, y)
+
+yn :: Bool -> String
+yn = bool "No" "Yes"
+printYn :: Bool -> IO ()
+printYn = putStrLn . yn
+
+counts :: Ord a => [a] -> [(a, Int)]
+counts = map count . group . sort
+  where count xs = (head xs, length xs)
