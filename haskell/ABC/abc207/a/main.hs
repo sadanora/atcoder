828a15d9cebd52b7ce84aac860db4d16649e5268
sadanora <sadanora.pi@gmail.com>
2025-06-17 20:19:24 +0900

    abc207a

---
+++ haskell/ABC/abc207/a/main.hs
@@ -0,0 +1,16 @@
+-- stack script --resolver lts-22 --package bytestring --package vector --package containers
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+import qualified Data.ByteString.Char8 as BS
+import Data.List
+import Data.Ord
+
+main :: IO ()
+main = do
+  xs <- getInts
+  print . sum . take 2 . sortBy (comparing Down) $ xs
+
+{-- IO --}
+getInts :: IO [Int]
+getInts = toInt <$> BS.getLine
+toInt :: BS.ByteString -> [Int]
+toInt = map (read . BS.unpack) . BS.words
