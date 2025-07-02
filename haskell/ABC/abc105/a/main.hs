7c502493dda706a732f025aea2ffd27dc1a9d590
sadanora <sadanora.pi@gmail.com>
2025-07-02 22:00:16 +0900

    NoviSteps 8Q58

---
+++ haskell/ABC/abc105/a/main.hs
@@ -0,0 +1,14 @@
+-- stack script --resolver lts-22 --package bytestring --package vector --package containers
+{-# OPTIONS_GHC -Wno-unused-top-binds -Wno-incomplete-patterns -Wno-missing-signatures -Wno-unused-matches #-}
+import qualified Data.ByteString.Char8 as BS
+
+main :: IO ()
+main = do
+  [n, k] <- getInts
+  print $ if n `mod` k == 0 then 0 else 1
+
+{-- IO --}
+getInts :: IO [Int]
+getInts = toInt <$> BS.getLine
+toInt :: BS.ByteString -> [Int]
+toInt = map (read . BS.unpack) . BS.words
