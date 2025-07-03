1b8a70c6fd0b6fda88f686ccfbe829551aeff8ee
sadanora <sadanora.pi@gmail.com>
2025-07-03 18:19:34 +0900

    NoviSteps 8Q58 abc088a

--- haskell/ABC/abc088/a/main.hs
+++ haskell/ABC/abc088/a/main.hs
@@ -1,7 +1,18 @@
+-- stack script --resolver lts-22 --package bytestring --package vector --package containers
+{-# OPTIONS_GHC -Wno-unused-top-binds -Wno-incomplete-patterns -Wno-missing-signatures -Wno-unused-matches #-}
+import qualified Data.ByteString.Char8 as BS
+import Data.Bool (bool)
+
 main :: IO ()
 main = do
-
+  n <- getInt
+  a <- getInt
+  printYn $ 0 >= ((n `mod` 500) - a)
 
 {-- IO --}
-getInts :: IO [Int]
-getInts = map (read :: String -> Int) . words <$> getLine
+getInt :: IO Int
+getInt = read . BS.unpack <$> BS.getLine
+yn :: Bool -> String
+yn = bool "No" "Yes"
+printYn :: Bool -> IO ()
+printYn = putStrLn . yn
