5f6f277652c95036af42d219a72c284fa378c4e4
sadanora <sadanora.pi@gmail.com>
2025-07-10 22:41:05 +0900

    NoviSteps 8Q58 abc066a

---
+++ haskell/ABC/abc066/a/main.hs
@@ -0,0 +1,16 @@
+-- stack script --resolver lts-22 --package bytestring --package vector --package containers
+{-# OPTIONS_GHC -Wno-unused-top-binds -Wno-incomplete-patterns -Wno-missing-signatures -Wno-unused-matches #-}
+import qualified Data.ByteString.Char8 as BS
+import Data.List
+import Data.Ord
+
+main :: IO ()
+main = do
+  abc <- getInts
+  print . sum . tail . sortBy (comparing Data.Ord.Down) $ abc
+
+{-- IO --}
+getInts :: IO [Int]
+getInts = toInt <$> BS.getLine
+toInt :: BS.ByteString -> [Int]
+toInt = map (read . BS.unpack) . BS.words
