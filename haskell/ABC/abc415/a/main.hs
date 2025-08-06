57cce6fef4c94f6492991a9726b0825ca6dff05a
sadanora <sadanora.pi@gmail.com>
2025-08-06 22:19:46 +0900

    NoviSteps 8Q

---
+++ haskell/ABC/abc415/a/main.hs
@@ -0,0 +1,23 @@
+-- stack script --resolver lts-22 --package bytestring --package vector --package containers
+{-# OPTIONS_GHC -Wno-unused-top-binds -Wno-incomplete-patterns -Wno-missing-signatures -Wno-unused-matches #-}
+import qualified Data.ByteString.Char8 as BS
+import Data.Bool
+
+main :: IO ()
+main = do
+  n <- getInt
+  as <- getInts
+  x <- getInt
+  printYn $ elem x as
+
+{-- IO --}
+getInt :: IO Int
+getInt = read . BS.unpack <$> BS.getLine
+getInts :: IO [Int]
+getInts = toInt <$> BS.getLine
+toInt :: BS.ByteString -> [Int]
+toInt = map (read . BS.unpack) . BS.words
+yn :: Bool -> String
+yn = bool "No" "Yes"
+printYn :: Bool -> IO ()
+printYn = putStrLn . yn
