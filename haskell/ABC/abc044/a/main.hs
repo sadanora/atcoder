8b8f44ab08268b74a093b7962aa6419a46f83245
sadanora <sadanora.pi@gmail.com>
2025-08-20 23:28:21 +0900

    NoviSteps 8Q abc044a

---
+++ haskell/ABC/abc044/a/main.hs
@@ -0,0 +1,15 @@
+-- stack script --resolver lts-22 --package bytestring --package vector --package containers
+{-# OPTIONS_GHC -Wno-unused-top-binds -Wno-incomplete-patterns -Wno-missing-signatures -Wno-unused-matches #-}
+import qualified Data.ByteString.Char8 as BS
+
+main :: IO ()
+main = do
+  n <- getInt
+  k <- getInt
+  x <- getInt
+  y <- getInt
+  print $ (min n k * x) + (max (n-k) 0 * y)
+
+{-- IO --}
+getInt :: IO Int
+getInt = read . BS.unpack <$> BS.getLine
