66c2b1cf8074ce72cdb788740af891756f2ac800
sadanora <sadanora.pi@gmail.com>
2025-10-26 13:55:07 +0900

    tessoku-book a13

--- ruby/Other_Contests/tessoku-book/a13/main.rb
+++ ruby/Other_Contests/tessoku-book/a13/main.rb
@@ -1,1 +1,19 @@
-input = gets.split.map(&:to_i)
+n, k = gets.split.map(&:to_i)
+arr = [0] + gets.split.map(&:to_i)
+r = [0]*(10**5)
+ans = 0
+(1..n-1).each do |i|
+  if i == 1
+    r[i] += 1
+  else
+    r[i] = r[i-1]
+  end
+
+  while (r[i] < n) && (arr[r[i]+1]-arr[i] <= k)
+    r[i] += 1
+  end
+end
+(1..n-1).each do |i|
+  ans += r[i] - i
+end
+p ans
