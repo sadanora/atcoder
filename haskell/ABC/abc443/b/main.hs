5352f873813184f21411dfffd02197efd6cb113d
sadanora <sadanora.pi@gmail.com>
2026-02-02 20:10:27 +0900

    abc443

---
+++ haskell/ABC/abc443/b/main.hs
@@ -0,0 +1,15 @@
+-- stack script --resolver lts-22 --package bytestring --package vector --package containers
+{-# OPTIONS_GHC -Wno-unused-top-binds -Wno-incomplete-patterns -Wno-missing-signatures -Wno-unused-matches #-}
+import qualified Data.ByteString.Char8 as BS
+import Data.List
+import Data.Maybe
+
+main :: IO ()
+main = do
+  [n, k] <- getInts
+  print $ fromJust $ findIndex (k <=) $ scanl1 (+) [n..]
+
+getInts :: IO [Int]
+getInts = toInt <$> BS.getLine
+toInt :: BS.ByteString -> [Int]
+toInt = map (read . BS.unpack) . BS.words
